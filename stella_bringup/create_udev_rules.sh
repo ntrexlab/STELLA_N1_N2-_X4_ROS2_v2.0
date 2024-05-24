1128e42cf42c057942e6efc4e86bd2d88738ebf6
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2024-05-24 13:32:04 +0900

    Humble stella_bringup

---
+++ stella_bringup/create_udev_rules.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "remap the devices serial port(ttyUSBX, ttySX) to  ydlidar, AHRS, Motordriver, Bluetooth"
+echo "devices usb connection as /dev/YDLIDAR, /dev/AHRS, /dev/MW, /dev/BT  , check it using the command : ls -l /dev|grep -e ttyUSB -e ttyTHS1"
+echo "start copy stella.rules to  /etc/udev/rules.d/"
+echo "/home/odroid/colcon_ws/src/STELLA_N1_ODROID_N2_X4_ROS2_v2.0/stella_bringup/stella.rules"
+sudo cp /home/odroid/colcon_ws/src/STELLA_N1_ODROID_N2_X4_ROS2_v2.0/stella_bringup/stella.rules  /etc/udev/rules.d
+echo " "
+echo "Restarting udev"
+echo ""
+sudo udevadm trigger
+echo "finish "
